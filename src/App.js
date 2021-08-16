c1c4ada6cfb1d3f2443c89a82e14fe28069bdb2c
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-16 16:50:01 +0530

    Symptons Section has been started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Hero from "./Components/Hero/Hero Section/Hero";
 import Terms from "./Components/Terms/Terms";
+import Symptoms from "./Components/AddSymptoms/Symptoms";
 
 function App() {
   const [start, setStart] = useState(false);
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Hero start={start} setStart={setStart} />
       <Terms start={start} setStart={setStart} />
+      <Symptoms/>
     </div>
   );
 }
---
+++ src/Components/AddSymptoms/Symptoms.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Symptoms.css";
+
+function Symptoms() {
+  return (
+    <div>
+      <hr className="line" />
+      <div className="symptoms-container">
+        <div className="header">Add Symptoms</div>
+      </div>
+    </div>
+  );
+}
+
+export default Symptoms;
