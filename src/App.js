d2611eee9e46f7f5946ae322e036daa83c4c2c47
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-16 15:38:44 +0530

    Conditional Rendering done for terms sections

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css'
-import {useState} from 'react'
-import Hero from './Components/Hero/Hero Section/Hero'
-import Terms from './Components/Terms/Terms'
+import "./App.css";
+import { useState } from "react";
+import Hero from "./Components/Hero/Hero Section/Hero";
+import Terms from "./Components/Terms/Terms";
 
 function App() {
-
-  const[start, setStart] = useState(true)
+  const [start, setStart] = useState(false);
 
   return (
     <div className="App">
-      <Hero start={start} setStart={setStart}/>
-      <Terms start={start} setStart={setStart}/>
+      <Hero start={start} setStart={setStart} />
+      <Terms start={start} setStart={setStart} />
     </div>
   );
 }
--- src/Components/Terms/Terms.jsx
+++ src/Components/Terms/Terms.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Terms.css";
 
-function Terms({start,setStart}) {
-  return (
-    <div className="terms-container">
-      <hr className="terms-line"/>
-      <div className="terms">
-        "I have read, understood and I accept and agree to comply with the Terms
-        of Use of EndlessMedicalAPI and Endless Medical services. The Terms of
-        Use are available on endlessmedical.com"
+function Terms({ start, setStart }) {
+  if (start) {
+    return (
+      <div className="terms-container">
+        <hr className="terms-line" />
+        <div className="terms">
+          "I have read, understood and I accept and agree to comply with the
+          Terms of Use of EndlessMedicalAPI and Endless Medical services. The
+          Terms of Use are available on endlessmedical.com"
+        </div>
+        <button onClick={() => setStart(false)} className="agree-button">
+          Agree
+        </button>
       </div>
-      <button onClick={() => setStart(true)} className="agree-button">Agree</button>
-    </div>
-  );
+    );
+  }
+  else{
+    return(
+      <div></div>
+    );
+  }
 }
 
 export default Terms;
