33c8ce7663b8ac6b0dcc9a98c22ccc51759b2a65
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-17 19:07:53 +0530

    Table Styling done but not perfect

--- src/Components/SymDisplay/SymDisplay.jsx
+++ src/Components/SymDisplay/SymDisplay.jsx
@@ -5,19 +5,21 @@ function SymDisplay({ diagfeatures, setDiagfeatures }) {
   return (
     <div>
       <div className="symdisplay-container">
-        <div className="top-bar">
-          <p className="heading-text">Sl.No</p>
-          <p className="heading-text">Symptoms</p>
-          <p className="heading-text">Values</p>
-        </div>
-
-        {diagfeatures.map((diagfeature, index) => (
-          <div className="value-bar">
-            <p className="values-text">{index+1}</p>
-            <p className="values-text">{diagfeature.symptom}</p>
-            <p className="values-text">{diagfeature.value}</p>
+        <div className="table-container">
+          <div className="top-bar">
+            <p className="heading-text">Sl.No</p>
+            <p className="heading-text">Symptoms</p>
+            <p className="heading-text">Values</p>
           </div>
-        ))}
+
+          {diagfeatures.map((diagfeature, index) => (
+            <div className="value-bar">
+              <p className="values-text">{index + 1}</p>
+              <p className="values-text">{diagfeature.symptom}</p>
+              <p className="values-text">{diagfeature.value}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
